7f0c410e5a65a17b2180c097bab18c3f0455e468
Cylon <cylon.unifg@gmail.com>
2023-01-12 15:22:10 -0300

    operado 1

---
+++ OrangeTech/src/OrangeTech.java
@@ -0,0 +1,18 @@
+public class OrangeTech {
+    public static void main(String[] args) throws Exception {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        String meuNome = "Cylon";
+
+
+        double soma = 10.5 + 15.7;
+        int subtração = 113-25;
+        int multiplicação = 20*7;
+        int divisão = 15/3;
+        int modulo = 18 % 3;
+        double resultado = (10*7)+(20/4);
+
+        String nomeCompleto = "Francisco"+"Cipriano"
+        System.out.println(nomeCompleto);
+    }
+}
