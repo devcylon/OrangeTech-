6f5d339e1923f2cc7e989b49983c0bc668991426
Cylon <cylon.unifg@gmail.com>
2023-01-12 15:47:23 -0300

    operadores 2

--- OrangeTech/src/OrangeTech.java
+++ OrangeTech/src/OrangeTech.java
@@ -3,7 +3,7 @@ public class OrangeTech {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         String meuNome = "Cylon";
-
+        final double VALOR_DE_PI = 3.14F;
 
         double soma = 10.5 + 15.7;
         int subtração = 113-25;
@@ -12,7 +12,12 @@ public class OrangeTech {
         int modulo = 18 % 3;
         double resultado = (10*7)+(20/4);
 
-        String nomeCompleto = "Francisco"+"Cipriano"
-        System.out.println(nomeCompleto);
+        String nomeCompleto = "Francisco"+" Cipriano ";
+        System.out.println(nomeCompleto + VALOR_DE_PI);
+        int a = 5;
+        int b = 5;
+
+        String resultado1 = a==b? "Verdadeiro" :"Falso";
+        System.out.println(resultado1);
     }
 }
