bee927ea256e12f74cc59b3e4d383e61c1a2098c
Cylon <cylon.unifg@gmail.com>
2023-01-20 09:01:03 -0300

    DesafioLetuDeGertrudes

---
+++ OrangeTech/src/Desafios/LeituraDaGertrudes.java
@@ -0,0 +1,13 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class LeituraDaGertrudes {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int paginas = leitor.nextInt();
+        int paginasLidas = 3;
+        // TODO: Crie a condição necessária para que Getrudes saiba em quanto tempo terminará seus livros
+        System.out.println((paginas/paginasLidas)+ " dias");
+    }
+}
